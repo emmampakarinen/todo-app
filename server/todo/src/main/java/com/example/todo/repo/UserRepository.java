2ff619a77f6eef8aae7c6248466c29b8b61af84a
Emma Pakarinen <emma.m.pakarinen@gmail.com>
2025-09-13 13:38:54 +0300

    move files up one level

--- server/todo/src/main/java/com/example/todo/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.todo.repo;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.todo.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
